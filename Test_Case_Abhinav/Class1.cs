09436a0e8af2cc91dc854c2f29221e464a8f33d9
chauh <chauh@MERCEDES>
2024-06-10 17:12:29 -0400

    Add project files.

---
+++ Test_Case_Abhinav/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Unit_Test_Abhinav;
+namespace Test_Case_Abhinav
+{
+    [TestFixture]
+    public class TriangleTest1
+    {
+        // Test_Case_Abhinav
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            string expected = "The triangle is valid.";
+            // Act
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Unit_Test_Abhinav/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace Unit_Test_Abhinav
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
